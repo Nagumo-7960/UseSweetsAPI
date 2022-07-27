62b304b6f51aed3d0b10d91b87b7d30af213450e
sketchbook_01@icloud.com <sketchbook_01@icloud.com>
2022-07-27 21:59:49 +0900

    Sweets APIの情報の取得に失敗

--- UseSweetsAPI/UseSweetsAPI/ViewController.swift
+++ UseSweetsAPI/UseSweetsAPI/ViewController.swift
@@ -7,13 +7,63 @@
 
 import UIKit
 
+//Dataを構造体で受け取る
+struct Qiita:Codable{
+    let title:String
+    let url:String
+    let user: User
+    
+    enum CodingKeys:String, CodingKey{
+        case title = "title"
+        case url = "url"
+        case user = "user"
+    }
+}
+
+struct User:Codable{
+    let name:String
+    let profileImageUrl:String
+    
+    enum CodingKeys:String, CodingKey{
+        case name = "name"
+        case profileImageUrl = "profile_image_url"
+    }
+}
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        getQiitaAPI()
     }
 
 
 }
+
+private func getQiitaAPI(){
+    guard let url = URL(string: "http://sweetsapi.c.fun.ac.jp/sweets")else {return}
+    
+    var request = URLRequest(url: url)
+    request.httpMethod = "GET"
+    
+    let task = URLSession.shared.dataTask(with: url){(data, respose, err) in
+        if let err = err{
+            print("情報の取得に失敗しました。 :", err)
+            return
+        }
+        if let data = data{
+            do{
+                let qiita = try JSONDecoder().decode([Qiita].self, from: data)
+                print("json: ", qiita)
+
+            }catch(let err){
+                print("情報の取得に失敗しました。:", err)
+            }
+        }
+    }
+    
+    task.resume()
+}
+
+
 
